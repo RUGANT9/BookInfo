f433e0cf9f752316c18c24766d7f15910e918a01
Devdutt <doruganti@fortinet.com>
2021-09-14 12:10:52 +0530

    added crud methods

---
+++ crud/book.py
@@ -0,0 +1,55 @@
+from db.book import Book
+from typing import Optional, List
+from sqlalchemy.orm import Session
+
+
+def create():
+    pass
+
+
+def update():
+    pass
+
+
+def delete():
+    pass
+
+
+def get_by_id(
+    db_session: Session, *, id: int
+) -> Optional[Book]:
+    return (
+        db_session.query(Book)
+        .filter(Book.book_id == id)
+        .first()
+    )
+
+
+def get_by_book_name(
+    db_session: Session, *, name: str
+) -> Optional[List[Book]]:
+    return (
+        db_session.query(Book)
+        .filter(Book.book_name == name)
+        .all()
+    )
+
+
+def get_by_author_name(
+    db_session: Session, *, name: str
+) -> Optional[List[Book]]:
+    return (
+        db_session.query(Book)
+        .filter(Book.book_author == name)
+        .all()
+    )
+
+
+def get_by_publisher_name(
+    db_session: Session, *, name: str
+) -> Optional[List[Book]]:
+    return (
+        db_session.query(Book)
+        .filter(Book.book_publisher == name)
+        .all()
+    )
