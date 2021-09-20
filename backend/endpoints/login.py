84007b88ce02673f6a94c04a2e71b857caa1a387
Devdutt <doruganti@fortinet.com>
2021-09-20 17:06:50 +0530

    started building frontend and api endpoints

---
+++ backend/endpoints/login.py
@@ -0,0 +1,12 @@
+from fastapi import Depends, APIRouter
+from fastapi.security import OAuth2PasswordBearer
+
+router = APIRouter()
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+@router.get("/items/")
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
--- frontend/app/book-info/src/App.js
+++ frontend/app/book-info/src/App.js
@@ -1,19 +1,41 @@
-// import logo from './logo.svg';
-import './App.css';
 import React from 'react';
-
-
-class App extends React.Component {					
-	
-	render() {
-    return (
-      <div>
-        <h1><font bg = "black" color="gold">Welcome to Bookinfo(site under construction, stay tuned...)</font></h1>
-        <button type="button" underlayColor="gold" onClick={this.changeValue}>Login</button>
-        <button type="button" underlayColor="gold" onClick={this.changeValue}>Sign up</button>
-      </div>
-    );
-  }
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import SignUp from './pages/signup';
+  
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/sign-up' component={SignUp} />
+      </Switch>
+    </Router>
+  );
 }
-
+  
 export default App;
+// import logo from './logo.svg';
+// import './App.css';
+// import React from 'react';
+
+
+// class App extends React.Component {					
+	
+// 	render() {
+//     return (
+//       <div>
+//         <h1><font bg = "black" color="gold">Welcome to Bookinfo(site under construction, stay tuned...)</font></h1>
+//         <button type="button" underlayColor="gold" onClick={this.changeValue}>Login</button>
+//         <button type="button" underlayColor="gold" onClick={this.changeValue}>Sign up</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
